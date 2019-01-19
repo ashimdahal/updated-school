253a9172b42e59ca86a2460189e4e4a746512604
ashimdahal <bboxchmpx@gmail.com>
2019-01-19 12:11:08 +0545

    database

--- school.sql
+++ school.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2019 at 12:38 PM
+-- Generation Time: Jan 19, 2019 at 07:24 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
